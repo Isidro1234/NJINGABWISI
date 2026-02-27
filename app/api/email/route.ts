f99c29e9b931dce85ab88710c7f83f00c5a00235
Isidro1234 <skillsizy@gmail.com>
2026-02-27 08:03:20 -0600

    new

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -234,7 +234,7 @@ export async function POST(request:Request){
         const data = await request.json();
         const {email} = data;
         const generatecode = Math.floor(Math.random() * 999) + 1000;
-        sendEmails(generatecode, email)
+        await sendEmails(generatecode, email)
         console.log(generatecode)
         return NextResponse.json({res:generatecode, message:'success'}); 
     } catch (error) {
