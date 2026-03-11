4f222808f2cf005431858e5bed98eb7acbb512be
Isidro1234 <skillsizy@gmail.com>
2026-03-11 01:05:38 -0500

    new

--- app/portal/propriedades/page.tsx
+++ app/portal/propriedades/page.tsx
@@ -1,3 +1,5 @@
+
+"use client"
 import CustomCard from '@/components/custom/CustomCard'
 import CustomPCard from '@/components/custom/CustomPCard'
 import UIPprint from '@/components/custom/UIPprint'
@@ -11,7 +13,7 @@ export default function Propriedades() {
       <CustomCard bg={'#E3EAFA'} icon={<Image alt='icon' src={'/icons/imovel.svg'} 
       width={30} height={30}/>} description='bwisi proprieties' title='Minhas Propriedades'>
         <VStack   alignItems={'flex-start'} width={'100%'} height={'100%'} paddingTop={5} gap={4}>
-            <CustomPCard/>
+            <CustomPCard estado='' image='' name='' location='' />
         </VStack>
       </CustomCard>
       <UIPprint ref={null}/>
